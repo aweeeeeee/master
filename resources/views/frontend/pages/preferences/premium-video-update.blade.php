f33b6700bd335f6e737c00e812d62609dd976dd2
Norman Bicomong <normanb@SourcefitMNL.com>
2018-11-19 14:38:35 +0800

    update premium video - update template

--- resources/views/frontend/pages/preferences/premium-video-update.blade.php
+++ resources/views/frontend/pages/preferences/premium-video-update.blade.php
@@ -9,7 +9,7 @@
         @endauth
         </div>
         <div class="col-lg-8 my-account-form">
-            <h3 class="my-account-title">Custom Video - Update Template</h3>
+            <h3 class="my-account-title">Premium - Update Template</h3>
             <table class="table-action mb-5" cellpadding="0" cellspacing="0" width="100%">
                 <tr class="bg-eae">
                     <td><b>Video #</b></td>
@@ -25,20 +25,22 @@
                     </td>
                 </tr>
             </table>
-            <h3 class="mb-4">Template Preferences</h3>
-            Main Template (1)
+            <h3 class="mb-4">Template Selection</h3>
+            <h3 class="font20 my-3">Main Frame</h3>
             <div class="row">
                 <div class="col p-0"><img class="img-fluid" src="{{ asset('storage/account/video-system-frame.jpg') }}" class="img-fluid"></div>
             </div>
-            <div class="d-flex flex-row-reverse">
-              <div class="p-2"><a href="">Change</a></div>
+            <div class="d-flex justify-content-between border-bot7">
+                              <div>&lt;Click to Expand&gt;</div>
+                              <div class="p-2"><a class="color-ff0033">Change</a></div>
             </div>
-            End Template (3)
+            <h3 class="font20 my-3">End Frame</h3>
             <div class="row">
                 <div class="col p-0"><img class="img-fluid" src="{{ asset('storage/account/video-system-end-frame.jpg') }}" class="img-fluid"></div>
             </div>
-            <div class="d-flex flex-row-reverse">
-              <div class="p-2"><a href="">Change</a></div>
+            <div class="d-flex justify-content-between">
+                              <div>&lt;Click to Expand&gt;</div>
+                              <div class="p-2"><a class="color-ff0033">Change</a></div>
             </div>
         </div>
     </div>
