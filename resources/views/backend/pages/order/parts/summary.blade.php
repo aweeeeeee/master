d99347284a088a2380769f8720b750fdfa116209
Norman Bicomong <normanb@SourcefitMNL.com>
2018-12-06 15:58:28 +0800

    restructure file

---
+++ resources/views/backend/pages/order/parts/summary.blade.php
@@ -0,0 +1,215 @@
+              <table class="tbl-summary">
+                <tr>
+                    <td>Summary of Video Parameters</td>
+                    <td></td>
+                    <td></td>
+                    <td class="text-center">QA</td>
+                    <td class="text-center">Time Stamp #</td>
+                </tr>
+                <tr>
+                    <td>Details</td>  
+                    <td class="p-0">
+                        <div class="d-flex justify-content-start tbl-col-3">
+                            <div>#Pictures <span>#00</span></div>
+                            <div>Template <span>#00</span></div>
+                            <div>Est. Duration <span>#00m:00s</span></div>
+                        </div>
+                    </td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Script</td>
+                    <td class="p-0">
+                        <select class="form-control">
+                            <option value="">< Compiled/Completed ></option>
+                        </select>
+                    </td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Opening Animation</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Main Frame Template Selected</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Colour Selected</td>
+                    <td class="p-0">
+                        <div class="d-flex justify-content-start tbl-col-2">
+                            <div class="w-100">#XXXColour</span></div>
+                            <div class="w-100">#XXXColour</span></div>
+                        </div>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>End Frame Template Selected</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Colour Selected</td>
+                    <td class="p-0">
+                        <div class="d-flex justify-content-start tbl-col-2">
+                            <div class="w-100">#XXXColour</span></div>
+                            <div class="w-100">#XXXColour</span></div>
+                        </div>
+                    </td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Closing Animation</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Voice Settings</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Voice Selected for this Video</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Music Settings</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Music Selected for this Video</td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                            <input type="checkbox" class="custom-control-input" id="customControlInline">
+                            <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+              </table>
+              <table class="tbl-qa mt-4">
+                <tr>
+                    <td>Quality Assessment</td>
+                    <td></td>
+                    <td class="text-center">QA</td>  
+                    <td class="text-center">Time Stamp #</td>  
+                </tr>
+                <tr>
+                    <td>
+                        <div class="row">
+                            <div class="col-sm text-center">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                                </div>
+                                <div>Poor</div>
+                            </div>
+                            <div class="col-sm text-center">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                                </div>
+                                <div>Acceptable</div>
+                            </div>
+                            <div class="col-sm text-center">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                                </div>
+                                Average
+                            </div>
+                            <div class="col-sm text-center">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                                </div>
+                                Good
+                            </div>
+                            <div class="col-sm text-center">
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                                </div>
+                                Great
+                            </div>
+                        </div>     
+                    </td>
+                    <td></td>
+                    <td>
+                        <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                    <label class="custom-control-label" for="customControlInline"></label>
+                        </div>
+                    </td>
+                    <td></td>
+                </tr>
+              </table>
+              
+              <div class="tbl-title mt-3">Comments + Notes</div>
+              <div class="tbl-notes">
+              
+              </div>
