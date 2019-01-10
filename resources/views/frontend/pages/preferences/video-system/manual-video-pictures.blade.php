c520a1270a2d5cf5e4cf408b0c2cf2659c2cfd8d
Norman Bicomong <normanb@SourcefitMNL.com>
2019-01-10 15:28:26 +0800

    update video system step 1

--- resources/views/frontend/pages/preferences/video-system/manual-video-pictures.blade.php
+++ resources/views/frontend/pages/preferences/video-system/manual-video-pictures.blade.php
@@ -13,13 +13,68 @@
             <div class="border-bot5">
                 @include('frontend.pages.preferences.video-system.steps')
             </div>
-               <h5 class="color-414142 my-3"><b>Upload Your Pictures</b></h5>
+                <form>
+                <h3 class="font-t-ms font16 color-424244 my-3"><b>Property Address</b></h3>
+                <div class="row m-r-l">
+                    <div class="col-sm-10">
+                        <div class="d-flex flex-column register-form pt-2">
+                            <div><input type="text" class="form-control" name="" placeholder="Address 1" required></div>
+                            <div class="my-3"><input type="text" class="form-control" name="" placeholder="Address 2" required></div>
+                            <div><input type="text" class="form-control" name="" placeholder="Suburb" required></div>
+                            <div class="my-3">
+                                 <div class="row m-r-l my-0">
+                                     <div class="col-sm"><input type="text" class="form-control" name="" placeholder="State" required></div>
+                                     <div class="col-sm-3"><input type="text" class="form-control" name="" placeholder="Post Code" required></div>
+                                 </div>
+                            </div>
+                        </div>
+                        <h3 class="font-t-ms font16 color-424244"><b>Property Features</b></h3>
+                        <div class="d-flex flex-column register-form pt-2">
+                            <div>
+                                <div class="row m-r-l my-0">
+                                    <div class="col-sm">
+                                        <div class="d-flex flex-row">
+                                            <div class="bg-eae px-2 pt-2"><i class="d-u-icon"></i></div>
+                                            <input type="text" name="" class="form-control b-radius-0 ml-2 w-100" placeholder="Bedroom">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm">
+                                         <div class="d-flex flex-row">
+                                             <div class="bg-eae p-2"><i class="d-u-icon d-u-icon-bat"></i></div>
+                                             <input type="text" name="" class="form-control b-radius-0 ml-2 w-100" placeholder="Bathrooms">
+                                         </div>
+                                    </div>
+                                    <div class="col-sm">
+                                         <div class="d-flex flex-row">
+                                             <div class="bg-eae p-2"><i class="d-u-icon d-u-icon-car"></i></div>
+                                             <input type="text" name="" class="form-control b-radius-0 ml-2 w-100" placeholder="Cars">
+                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="register-form mb-3">
+                                 <select name="suburb" class="form-control">
+                                    <option disabled selected>Property Type</option>
+                                    <option>House</option>
+                                    <option>Unit</option>
+                                    <option>Townhouse</option>
+                                    <option>Villa</option>
+                                    <option>Duplex</option>
+                                    <option>Apartment</option>
+                                    <option>Rural</option>
+                                 </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </form>
+                <h3 class="font-t-ms font16 color-424244 my-3"><b>Upload Your Pictures</b></h3>
                <div class="direct-upload-4">
                     <div class="d-flex justify-content-between mx-3 mb-2 mt-3">
                         <div class="d-u-width">
                             <button class="btn btn-primary b-radius-0 w-100 py-2"><i class="standard-video sv-browse"></i> Browse</button>
                             <div class="d-flex justify-content-between color-666 mt-2">
-                                    <div>jpg or .png <br>size 200-600kb</div>
+                                    <div>.jpg or .png <br>size 200-600kb</div>
                                     <div>Minimum 10 <br>Maximum 25</div>
                             </div>
                         </div>
