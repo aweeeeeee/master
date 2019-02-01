4f3232c20c798b74664f1aa86f217e1c3051f151
Norman Bicomong <normanb@SourcefitMNL.com>
2019-02-01 10:13:15 +0800

    update example template

--- resources/views/backend/layouts/example-template.blade.php
+++ resources/views/backend/layouts/example-template.blade.php
@@ -29,8 +29,21 @@
                 </div>
             </div>
         </div>
+        <br><br>
     </div>
-    
+    <div class="d-flex flex-row peard-template-3">
+            <div class="align-self-end"><img src="{{ asset('storage/Jeff-Coghill.png') }}" class="img fluid"></div>
+            <div class="text-center align-self-center w-100">
+                <img src="{{ asset('storage/peard-logo.png') }}">
+                <div class="d-flex flex-column peard-color-3">
+                    <div class="my-5"><b>CONTACT ME <br>TODAY!</b></div>
+                    <div><b>JEFF COGHILL</b></div>
+                    <div>0412 928 643</div>
+                    <div>(08) 9270 0000</div>
+                    <div class="mt-5">www.peard.com.au</div>
+                </div>
+            </div>
+        </div>
 
 
 @endsection
