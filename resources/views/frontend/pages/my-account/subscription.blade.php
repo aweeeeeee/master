18a84601a41f1fb6672886f0084778b28f9e1c1a
Norman Bicomong <normanb@SourcefitMNL.com>
2019-06-14 14:16:27 +0800

    update my account - subcription

--- resources/views/frontend/pages/my-account/subscription.blade.php
+++ resources/views/frontend/pages/my-account/subscription.blade.php
@@ -1,94 +1,51 @@
 <div class="border-bot7 pb-2">
 <h3 class="mb-4">Subscription + Storage Plan</h3>
+            <div class="row my-account-subcription col-10 pl-0 m-r-l mb-3">
+              <div class="col-md"><button type="button" class="btn basic p-2 b-radius-7 ">Basic</button></div>
+              <div class="col-md"><button type="button" class="btn p-2 per-month b-radius-7 ">$33/month</button></div>
+            </div>
             <p>Upgrade my Subscription + Storage Plan</p>
-            <h3 class="font-weight-bold mb-3">What Does REVid Cost?</h3>
-
-                            <table cellpadding="0" cellspacing="0" border="0" class="premium-table w-100">
-                                <tr>
-                                    <td>$99<sup>+gst</sup> per Property Listing</td>
-                                    <td>
-                                        <div class="d-flex justify-content-between pricing-data">
-                                            <div>Casual @ $0/month</div>
-                                            @if(!Request::is('/')) 
-                                                <div class="custom-control custom-checkbox mt-1">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                                    <label class="custom-control-label font-weight-bold" for="customCheck1"></label>
-                                                </div>
-                                            @endif
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div class="d-flex justify-content-between pricing-data">
-                                            <div>Premium @ $33/month</div>
-                                            @if(!Request::is('/')) 
-                                                <div class="custom-control custom-checkbox mt-1">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck2">
-                                                    <label class="custom-control-label font-weight-bold" for="customCheck2"></label>
-                                                </div>
-                                            @endif
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><b>Main Video in Fullscreen</b>
-                                        <div>90 Seconds</div>
-                                    </td>
-                                    <td><b>Yes</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Main Video in Agent Frame</b>
-                                        <div>90 Seconds</div>
-                                    </td>
-                                    <td><b>Yes</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Voice-Over</b></td>
-                                    <td><b>Yes</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Subtitles</b></td>
-                                    <td><b>Yes</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Refresh Video</b>
-                                        <div>60 Seconds</div>
-                                    </td>
-                                    <td><b>No, extra $20</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Price Adjustment Video</b> 
-                                        <div>40 Seconds</div>
-                                    </td>
-                                    <td><b>No, extra $20</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Sold</b> (More Wanted) <b>Video</b>
-                                        <div>40 Seconds</div>
-                                    </td>
-                                    <td><b>No, extra $20</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td>One <b>FREE</b> Video
-                                    <div>EVERY 3 Months</div></td>
-                                    <td><b>No</b></td>
-                                    <td><b>Yes</b></td>
-                                </tr>
-                                <tr>
-                                    <td><b>Vendor Marketing Material</b>
-                                        <div>(Brochure + Video)</div>
-                                    </td>
-                                    <td><b>Generic Only</b></td>
-                                    <td><b>Personalised to Your Brand</b></td>
-                                </tr>
-                            </table>
-                             <div class="line-height20 my-3">
-                                <button type="submit" class="btn btn-primary b-radius-7 bg-ff6600 <!--bg-009900--> btn-no-border px-1 py-2"><div class="font15"><b>CHANGE SUBSCRIPTION</b></div></button>
+            <div class="d-flex flex-row-reverse c-premium">
+                    <div class="position-absolute">
+                        <img class="img-fluid" src={{ asset('storage/free-premium.png') }} />
+                    </div>
+            </div>
+            <div class="my-account-subcription">
+                <div class="row">
+                    <div class="col-7">
+                        <div class="c-subs">
+                            <div class="row">
+                                <div class="col pl-2 font-t-ms color-666"><b>Casual</b></div>
+                                <div class="col font-t-ms">$0/month</div>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="custom-control custom-checkbox casual chk-subscription">
+                          <input type="checkbox" class="custom-control-input" id="customCheck1">
+                          <label class="custom-control-label" for="customCheck1"></label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-7">
+                        <div class="p-subs">
+                            <div class="row">
+                                <div class="col pl-2 font-t-ms color-666"><b>Premium</b></div>
+                                <div class="col font-t-ms">$33/month</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="custom-control custom-checkbox premium chk-subscription">
+                          <input type="checkbox" class="custom-control-input" id="customCheck2">
+                          <label class="custom-control-label" for="customCheck2"></label>
+                        </div>
+                    </div>
+                </div>
+
+            <div class="line-height20 my-3">
+                <button type="submit" class="btn btn-primary b-radius-7 bg-ff6600 <!--bg-009900--> btn-no-border px-1 py-2"><div class="font15"><b>CHANGE SUBSCRIPTION</b></div></button>
+            </div>
+        </div>
 </div>
