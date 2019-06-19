b42e72ff70ef64ccd1cfe85a148aaef78f26f27a
Norman Bicomong <normanb@SourcefitMNL.com>
2019-06-19 10:19:21 +0800

    update my account subscription

--- resources/views/frontend/pages/my-account/subscription.blade.php
+++ resources/views/frontend/pages/my-account/subscription.blade.php
@@ -5,47 +5,54 @@
               <div class="col-md"><button type="button" class="btn p-2 per-month b-radius-7 ">$33/month</button></div>
             </div>
             <p>Upgrade my Subscription + Storage Plan</p>
-            <div class="d-flex flex-row-reverse c-premium">
-                    <div class="position-absolute">
-                        <img class="img-fluid" src={{ asset('storage/free-premium.png') }} />
-                    </div>
-            </div>
             <div class="my-account-subcription">
                 <div class="row">
                     <div class="col-7">
-                        <div class="c-subs">
-                            <div class="row">
-                                <div class="col pl-2 font-t-ms color-666"><b>Casual</b></div>
-                                <div class="col font-t-ms">$0/month</div>
+                        <div class="row">
+                            <div class="col px-0">
+                                <div class="c-subs">
+                                    <div class="row">
+                                        <div class="col pl-2 font-t-ms color-666"><b>Casual</b></div>
+                                        <div class="col font-t-ms">$0/month</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <div class="custom-control custom-checkbox casual chk-subscription">
+                                  <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                  <label class="custom-control-label" for="customCheck1"></label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col px-0">
+                                <div class="p-subs">
+                                    <div class="row">
+                                        <div class="col pl-2 font-t-ms color-666"><b>Premium</b></div>
+                                        <div class="col font-t-ms">$33/month</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <div class="custom-control custom-checkbox premium chk-subscription">
+                                  <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                  <label class="custom-control-label" for="customCheck2"></label>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-4">
-                        <div class="custom-control custom-checkbox casual chk-subscription">
-                          <input type="checkbox" class="custom-control-input" id="customCheck1">
-                          <label class="custom-control-label" for="customCheck1"></label>
+                    <div class="col-auto">
+                         <div class="d-flex flex-row-reverse c-premium">
+                                <div class="">
+                                    <img class="img-fluid" src={{ asset('storage/free-premium.png') }} />
+                                </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-7">
-                        <div class="p-subs">
-                            <div class="row">
-                                <div class="col pl-2 font-t-ms color-666"><b>Premium</b></div>
-                                <div class="col font-t-ms">$33/month</div>
-                            </div>
+                 <div class="line-height20 my-3">
+                            <button type="submit" class="btn btn-primary b-radius-7 bg-ff6600 <!--bg-009900--> btn-no-border px-1 py-2"><div class="font15"><b>CHANGE SUBSCRIPTION</b></div></button>
                         </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="custom-control custom-checkbox premium chk-subscription">
-                          <input type="checkbox" class="custom-control-input" id="customCheck2">
-                          <label class="custom-control-label" for="customCheck2"></label>
-                        </div>
-                    </div>
-                </div>
 
-            <div class="line-height20 my-3">
-                <button type="submit" class="btn btn-primary b-radius-7 bg-ff6600 <!--bg-009900--> btn-no-border px-1 py-2"><div class="font15"><b>CHANGE SUBSCRIPTION</b></div></button>
-            </div>
+            
         </div>
 </div>
