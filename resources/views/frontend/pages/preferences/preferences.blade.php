1df468dc0cf5f6149cb06b45e441a9860143e828
aweeeeeee <30914571+aweeeeeee@users.noreply.github.com>
2020-02-11 10:32:02 +0800

    update my preferences

--- resources/views/frontend/pages/preferences/preferences.blade.php
+++ resources/views/frontend/pages/preferences/preferences.blade.php
@@ -9,15 +9,35 @@
         @endauth
         </div>
         <div class="col-lg-8 my-account-form pb-5">
-            <h3 class="my-account-title mb-2">My Preferences</h3>
+            <h4 class="my-account-title mb-2">My Preferences</h4>
+            HOMEOPEN info SETTINGS
+            <div class="d-flex flex-row step-three-register">
+                <div class="col-md-5 px-0 py-3"><b>Register Your Attendance System</b></div>
+                <div class="col-md">
+                    <div class="d-flex flex-row">
+                        <div class="align-self-center col-2 py-1">
+                            On
+                        </div>
+                        <div class="custom-control custom-checkbox standard">
+                            <input type="checkbox" class="custom-control-input" id="customCheckOn">
+                            <label class="custom-control-label" for="customCheckOn"></label>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-row">
+                        <div class="align-self-center col-2 py-1">
+                            Off
+                        </div>
+                        <div class="custom-control custom-checkbox standard pl-0">
+                            <input type="checkbox" class="custom-control-input" id="customCheckOff">
+                            <label class="custom-control-label" for="customCheckOff"></label>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <form class="step-three-register register-form">
-               
-                @include('frontend.pages.preferences.preferences.surge')
                     
                 @include('frontend.pages.preferences.preferences.email')
-                    
-                @include('frontend.pages.preferences.preferences.broadcast')
-                
+
             </form>
         </div>
     </div>
